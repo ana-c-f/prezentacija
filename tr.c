91f613abbbef3948c5a809d6b265b5fd5941a392
ana-c-f <79380210+ana-c-f@users.noreply.github.com>
2021-02-21 13:28:32 +0100

    Add files via upload

---
+++ tr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+
+int a,b,c;
+	
+	printf("Unesite duzine stranica trougla: \n");
+	scanf("%f","%f","%f", &a,&b,&c);
+	
+	float o=a+b+c;
+	float s=o / 2
+	float p=sqrt(s*(s-a)*(s-b)*(s-b))
+	
+	
+	prinf("Obim: %f \n", o);
+    printf("Povrsina: %f \n" , p);
+
+return 0;}
---
+++ trougao.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+
+float a,b,c;
+float o,s,p;
+	
+	printf("Unesite duzine stranica trougla: \n");
+	scanf("%f","%f","%f", &a,&b,&c);
+	
+	float o=(a+b+c);
+	float s=(o/2);
+	float p=(sqrt(s*(s-a)*(s-b)*(s-b)));
+	
+	
+	prinf("Obim: %f \n", o);
+    printf("Povrsina: %f \n" , p);
+
+return 0;}
